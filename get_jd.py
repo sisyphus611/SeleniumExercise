0ee5f89fca69d346e98dbfe9cc89a40c0bcec753
yournamesisyphus61 <username@example.com>
2018-07-23 15:03:41 +0800

    update 0.11

--- get_jd.py
+++ get_jd.py
@@ -1,52 +1,97 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from urllib.parse import quote
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from lxml import etree
+import pymongo
+import time
 
 browser = webdriver.Chrome()
-wait = WebDriverWait(browser, 10)
 
-KEYWORD = '耳机'
 
-def page_turn(page):
-    print('正在爬取第', page, '页')
+def jd_search():
+    '''
+    获取商品页数
+    '''
     try:
-        url = 'https://search.jd.com/Search?keyword=' + quote(KEYWORD) + '&enc=utf-8'
-        browser.get(url)
-        browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        if page > 1:
-            #input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > input')))
-            #submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > a')))
-            #input.clear()
-            #input.send_keys(page)
-            #submit.click()
-            input = browser.find_element_by_css_selector('#J_bottomPage > span.p-skip > input')
-            submit = browser.find_element_by_css_selector('#J_bottomPage > span.p-skip > a')
-            input.clear()
-            input.send_keys(page)
-            submit.click()
+        browser.get('https://www.jd.com/')
+        # 引入WebDriverWait对象，指定等待最长时间
+        wait = WebDriverWait(browser, 10)
+        # 调用WebDriverWait对象的until方法，以节点的定位元组形式传入等待条件
+        input = wait.until(EC.presence_of_element_located((By.ID, 'key')))
+        input.send_keys('ipad')
+        button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.button')))
+        button.click()
+        total = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > em:nth-child(1) > b')))
+        return total.text
+    except TimeoutError:
+        print('---error----')
 
 
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#J_bottomPage > span.p-num > a.curr'), str(page)))
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#J_goodsList > ul > li')))
-        get_infos()
-    except TimeoutException:
-        page_turn(page)
+def next_page():
+    '''
+    模拟点击下一页
+    '''
+    try:
+        wait = WebDriverWait(browser, 10)
+        button = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_bottomPage > span.p-num > a.pn-next > em')))
+        button.click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#J_goodsList > ul')))
+        html = browser.page_source
+        parse_item(html)
+    except TimeoutError:
+        print('----error---')
 
-def get_infos():
-    html = browser.page_source
-    infos = etree.HTML(html)
-    titles = infos.xpath('//*[@id="J_goodsList"]/ul/li/div/div[3]/strong/i/text()')
-    for title in titles:
-        print(title)
 
-MAX_PAGE = 10
+def parse_item(html_data):
+    '''
+    Xpath解析页面商品信息
+    '''
+    page_infos = etree.HTML(html_data)
+    items = page_infos.xpath('//*[@id="J_goodsList"]/ul/li')
+    for item in items:
+        try:
+            seller = item.xpath('./div/div[5]/span/a/@title')[0]
+        except IndexError:
+            seller = '空'
+
+        product = {
+            'title': item.xpath('./div/div[1]/a/@title')[0],
+            'price': item.xpath('./div/div[2]/strong/i/text()')[0],
+            'seller': seller,
+            'commit': item.xpath('./div/div[4]/strong/a/text()')[0]
+        }
+        save_info(product)
+
+
+def save_info(result):
+    '''
+    爬取的数据存入MongoDB数据库
+    '''
+
+    client = pymongo.MongoClient(host='localhost', port=27017)
+    db = client['mydb']
+    try:
+        if db['jd_products'].insert(result):
+            print('存储成功')
+    except Exception:
+        print('存储失败')
+
+
 def main():
-    for i in range(1, MAX_PAGE + 1):
-        page_turn(i)
+    '''
+    构造翻页函数
+    :return:
+    '''
+    total_num = jd_search()
+    print(total_num)
+    for page_num in range(2, int(total_num)):
+        time.sleep(3)
+        print('开始爬取第', page_num, '页')
+        next_page()
+
 
 if __name__ == '__main__':
     main()
